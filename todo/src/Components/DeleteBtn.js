acdbb3f56fd4220c99e267c7a8763d8e77920e72
AnnaxGrace <anna.grace.conover@gmail.com>
2021-02-01 14:11:14 -0600

    added delete component that renders with the list items

---
+++ todo/src/Components/DeleteBtn.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Button from "react-bootstrap/Button";
+
+
+function DeleteBtn() {
+    return (
+        <>
+           <Button id="delete-btn" variant="danger">Delete</Button>
+        </>
+    )
+}
+
+export default DeleteBtn;
+
--- todo/src/Components/Todo.jsx
+++ todo/src/Components/Todo.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
+import DeleteBtn from "./DeleteBtn";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 
 function Todo(props) {
-    return (
-        <>
-            <li>{props.item}</li>
-        </>
-    )
+  return (
+    <>
+      <li>
+        <Row>
+          <Col>{props.item}</Col>
+          <Col>
+            <DeleteBtn />
+          </Col>
+        </Row>
+      </li>
+    </>
+  );
 }
 
 export default Todo;
-
