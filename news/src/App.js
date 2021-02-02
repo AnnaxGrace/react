13b73eafea437743aa5a8164bc2f15e0bbe5b1c5
AnnaxGrace <anna.grace.conover@gmail.com>
2021-02-02 17:20:02 -0600

    adding Router/Link to replace the a tags

--- news/src/App.js
+++ news/src/App.js
@@ -1,29 +1,38 @@
-import logo from './tek_logo.png';
-import './App.css';
-import NavBar from './Components/NavBar';
-import Headlines from './Views/Headlines';
-import Sports from './Views/Sports';
-import Weather from './Views/Weather';
-import Politics from './Views/Politics';
-import Footer from './Components/Footer';
+import logo from "./tek_logo.png";
+import { HashRouter, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import Headlines from "./Views/Headlines";
+import Sports from "./Views/Sports";
+import Weather from "./Views/Weather";
+import Politics from "./Views/Politics";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavBar/>
+      <HashRouter basename="/">
+        <NavBar />
         <img src={logo} className="App-logo" alt="logo" />
-        <div>
-         <br/>
+        {/* <div>
+          <br />
           <p>Create a News Site</p>
-          <p>Set up a Single Page Application (SPA) with react-router</p>
-          <Headlines/>
-          <Sports/>
-          <Weather/>
-          <Politics/>
-        </div>
+          <p>Set up a Single Page Application (SPA) with react-router</p> */}
+            <div>
+              <Route exact path="/" component={Headlines} />
+              <Route exact path="/Sports" component={Sports} />
+              <Route exact path="/Weather" component={Weather} />
+              <Route exact path="/Politics" component={Politics} />
+            </div>
+          </HashRouter>
+          {/* <Headlines />
+          <Sports />
+          <Weather />
+          <Politics /> */}
+        {/* </div> */}
       </header>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
