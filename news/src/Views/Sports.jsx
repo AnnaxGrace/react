e8d5b71b184d2cc90df71b42811b8eeb7579708e
AnnaxGrace <anna.grace.conover@gmail.com>
2021-02-03 12:18:23 -0600

    sports successfully passes data and renders the article component

--- news/src/Views/Sports.jsx
+++ news/src/Views/Sports.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import Article from '../Components/Article';
+import React, { useEffect, useState } from "react";
+import Article from "../Components/Article";
+import API from "../utils/API";
+import { selectTopFive } from "../utils/tools";
 
 function Sports() {
-    return (
-        <div>
-            <h1>Sports News goes here: </h1>
-            <Article/>
-            <Article/>
-            <Article/>
-        </div>
-    )
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    API.getSports().then((results) => {
+      console.log(results.data.articles);
+      setArticles(selectTopFive(results.data.articles));
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Sports News goes here: </h1>
+      {articles.map((article, i) => (
+        <Article
+          key={i}
+          title={article[i].title}
+          thumbnail={article[i].urlToImage}
+          content={article[i].content}
+        />
+      ))}
+    </>
+  );
 }
 
 export default Sports;
